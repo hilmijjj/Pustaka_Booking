a0780c53c91159266edf81c62b19aeaa1792b9b9
hilmijjj <19221330@bsi.ac.id>
2023-09-25 17:01:18 +0700

    menambahkan latihan1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index() 
     { 
         echo "<h1>Perkenalkan</h1>"; 
-        echo"Nama saya Imam Nawawi 
-            Saya tingga di daerah Ciputat 
+        echo"Nama saya hilmi 
+            Saya tingga di daerah Cikarang 
             olah raga yang saya sukai adalah 
             Bulutangkis"; 
     } 
