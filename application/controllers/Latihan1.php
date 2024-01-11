549d65b3df09acf12c0241a9990adc73894392b8
hilmijjj <19221330@bsi.ac.id>
2024-01-11 15:55:30 +0700

    menambahkan tugas

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,15 @@
+<?php 
+class Latihan1 extends CI_Controller 
+{ 
+    public function index() 
+    {
+     
+        echo "<h1>Perkenalkan</h1>"; 
+        echo "Nama  : HILMI <br>"; 
+        echo "NIM   : 19221330<br>"; 
+        echo "HP    : 081510941639<br>"; 
+        echo "Hobi  : bulutangkis<br>"; 
+        echo "alamat: bekasi,cikarang utara,Kp.kandang<br>"; 
+        echo "Umur  : 19"; 
+    } 
+} 
---
+++ application/controllers/contoh1.php
@@ -0,0 +1,53 @@
+<?php
+//contoh1
+//menambahkan syntak 
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";
+    }
+}
+
+//contoh2
+//menambahkan syntak 
+<?php 
+class Latihan1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "Selamat Datang.. selamat belajar Web Programming"; 
+        //$this->load->view('view-latihan1'); 
+    } 
+ 
+    public function penjumlahan($n1, $n2) 
+    { 
+        $this->load->model('Model_latihan1'); 
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2); 
+        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = " 
+.$hasil; 
+    } 
+} 
+
+//contoh3
+//menambahkan syntak 
+<?php 
+class Latihan1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "Selamat Datang.. selamat belajar Web Programming"; 
+        //$this->load->view('view-latihan1'); 
+    } 
+ 
+    public function penjumlahan($n1, $n2) 
+    { 
+        $this->load->model('Model_latihan1'); 
+ 
+        $data['nilai1'] = $n1; 
+        $data['nilai2'] = $n2; 
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2); 
+ 
+        $this->load->view('view-latihan1', $data); 
+    } 
+} 
